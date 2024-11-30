981ee17e9ea858b6c50473b80e599986b57e509e
JuniorZSS <108765560+JuniorZSS@users.noreply.github.com>
2024-11-30 23:02:57 +0300

    блок 2

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+var tasks []string
+
+func addTast() {
+	fmt.Print("Добавь новую задачу, введи её название: ")
+	var task string
+	fmt.Scanln(&task)
+	tasks = append(tasks, task)
+	fmt.Println("Задача добавлена!")
+}
+
 func main() {
 	fmt.Println("Приветствую, это начало разработки Task Manager CLI!")
 }
